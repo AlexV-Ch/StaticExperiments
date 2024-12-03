2ae7ab3fb76a2181bc6f88fb0bf9f835fc780965
auto_1 <alexch@yandex.ru>
2024-12-03 16:52:02 +0300

    Задание 6.3.1 - старт 1

---
+++ src/Product.java
@@ -0,0 +1,28 @@
+public class Product {
+    //Создайте класс Product с двумя final-полями — name и price,
+    //конструктором с двумя параметрами, с помощью которого можно
+    //задавать их значения, и двумя геттерами для этих полей.
+private final String name;
+private final double price;
+    //Создайте также в этом классе два статических поля: int count и int
+    //totalPrice — и пропишите увеличение их значений в конструкторе.
+
+    public static int count = 0;
+    public static int totalPrice = 0;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+        count = count +1;
+        totalPrice = totalPrice + price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+}
